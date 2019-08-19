bcb30e6ec5d5e82382b4f9fe53b4edf7f4a7d744
limq <limq@cgnfs.com.cn>
2019-08-19 17:31:30 +0800

    mvc login dir tree

--- datamodels/user.go
+++ datamodels/user.go
@@ -1,1 +1,11 @@
 package datamodels
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	ID        int64  `json:"id" form:"id"`
+	Firstname string `json:"firstname" form:"firstname"`
+}
