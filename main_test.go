c2296f411c9aa9e6d9ac990bfff4502fe6b9ee37
limq <limq@cgnfs.com.cn>
2019-08-19 16:37:20 +0800

    iris mvc hello world

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// terminal run: go test
 func TestMVCHelloWorld(t *testing.T) {
 	e := httptest.New(t, newApp())
 	e.GET("/").Expect().Status(httptest.StatusOK).ContentType("text/html", "utf-8").Body().Equal("<h1>Welcome</h1>")
