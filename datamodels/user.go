91cb1c11e732d5d27f8a039cc73cd50435b27743
mingquan.li <mingquan.li@5262.com>
2019-08-19 23:46:26 +0800

    mvc login

--- datamodels/user.go
+++ datamodels/user.go
@@ -6,6 +6,26 @@ import (
 )
 
 type User struct {
-	ID        int64  `json:"id" form:"id"`
-	Firstname string `json:"firstname" form:"firstname"`
+	ID             int64     `json:"id" form:"id"`
+	Firstname      string    `json:"firstname" form:"firstname"`
+	Username       string    `json:"username" form:"username"`
+	HashedPassword []byte    `json:"-" form:"-"`
+	CreatedAt      time.Time `json:"created_at" from:"created_at"`
+}
+
+func (u User) IsValid() bool {
+	return u.ID > 0
+}
+
+//根据用户输入 生成hash
+func GeneratePassword(userPassword string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+}
+
+//检查密码是否匹配
+func ValidatePassword(userPassword string, hashed []byte) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword(hashed, []byte(userPassword)); err != nil {
+		return false, err
+	}
+	return true, nil
 }
--- datasource/users.go
+++ datasource/users.go
@@ -1,1 +1,21 @@
 package datasource
+
+import (
+	"errors"
+	"mvc/datamodels"
+)
+
+type Engine uint32
+
+const (
+	Memory Engine = iota
+	Bolt
+	MySql
+)
+
+func loadUsers(engine Engine) (map[int64]datamodels.User, error) {
+	if engine != Memory {
+		return nil, errors.New("请使用map类型作为数据源")
+	}
+	return make(map[int64]datamodels.User), nil
+}
