45dca78c0b6df09a3045004efbb351cb32208785
limq <limq@cgnfs.com.cn>
2019-08-19 16:36:30 +0800

    iris mvc hello world

--- main.go
+++ main.go
@@ -1,84 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
 	"github.com/kataras/iris/mvc"
-	"github.com/kataras/iris/sessions"
 )
 
 func main() {
-	app := iris.New()
-	app.Logger().SetLevel("debug")
-	mvc.Configure(app.Party("/basic"), basicMVC)
+	app := newApp()
+	// http://localhost:8080
+	// http://localhost:8080/ping
+	// http://localhost:8080/hello
+	// http://localhost:8080/custom_path
 	app.Run(iris.Addr(":8080"))
+
 }
 
-func basicMVC(app *mvc.Application) {
-
-	//使用中间件
-	app.Router.Use(func(ctx iris.Context) {
-		ctx.Application().Logger().Infof("Path:%s", ctx.Path())
-		ctx.Next()
-	})
-
-	//注册绑定到控制器上的依赖，依赖可以是函数：参数为iris.Context、返回值是单纯值，也可以是一个结构体值
-	//Register dependencies which will be binding to the controller(s),
-	//can be either a function which accepts an iris.Context and returns a single value (dynamic binding)
-	//or a static struct value (service).
-	app.Register(
-		sessions.New(sessions.Config{}).Start,
-		&prefixedLogger{prefix: "DEV"},
-	)
-
-	//子控制器(basicController、basicSubController)有父控制器(*mvc.Application)中依赖的clone，故所有控制器访问同一个会话
-	app.Handle(new(basicController))
-	app.Party("/sub").Handle(new(basicSubController))
+func newApp() *iris.Application {
+	app := iris.New()
+	app.Use(recover.New())                      //可以从任何与http相关的panics中恢复
+	app.Use(logger.New())                       //请求记录到终端
+	mvc.New(app).Handle(new(ExampleController)) //控制器根路由路径"/"
+	return app
 }
 
-type LoggerService interface {
-	Log(string)
-}
-type prefixedLogger struct {
-	prefix string
-}
+type ExampleController struct{}
 
-func (s *prefixedLogger) Log(msg string) {
-	fmt.Printf("%s: %s\n", s.prefix, msg)
-}
-
-type basicController struct {
-	Logger  LoggerService
-	Session *sessions.Session
-}
-
-func (c *basicController) BeforeActivation(b mvc.BeforeActivation) {
-	b.Handle("GET", "/custom", "Custom")
-	//b.HandleMany("GET", "/custom custom2", "Custom") HandleMany 2019-07-11后新增的方法
-}
-
-func (c basicController) AfterActivation(a mvc.AfterActivation) {
-	if a.Singleton() {
-		panic("basicController should be stateless,a request-scoped,we have a 'Session' which depends on the context.")
+//Get service
+//Method GET
+//Resource:http://localhost:8080
+func (c *ExampleController) Get() mvc.Result {
+	return mvc.Response{
+		ContentType: "text/html",
+		Text:        "<h1>Welcome</h1>",
 	}
 }
 
-func (c *basicController) Get() string {
-	count := c.Session.Increment("count", 1)
-	body := fmt.Sprintf("Hello from basicController\nTotal vistors from you: %d", count)
-	c.Logger.Log(body)
-	return body
+//遵循命名规范
+// GetPing 服务
+// 请求方法:   GET
+// 请求资源路径: http://localhost:8080/ping
+func (c *ExampleController) GetPing() string {
+	return "pong"
 }
 
-func (c *basicController) Custom() string {
-	return "custom"
+//遵循命名规范
+// GetHello 服务
+// 请求方法:   GET
+// 请求资源路径: http://localhost:8080/hello
+func (c *ExampleController) GetHello() interface{} {
+	return map[string]string{"message": "Hello Iris!"}
 }
 
-type basicSubController struct {
-	Session *sessions.Session
+func (c *ExampleController) BeforeActivation(b mvc.BeforeActivation) {
+	anyMiddlewareHere := func(ctx iris.Context) {
+		ctx.Application().Logger().Warnf("Inside / custom_path")
+		ctx.Next()
+	}
+	//自定义路径 不遵循命名规范
+	b.Handle("GET", "/custom_path", "CustomHandlerWithoutFollowingTheNamingGuide", anyMiddlewareHere)
 }
 
-func (c basicSubController) Get() string {
-	count := c.Session.GetIntDefault("count", 1)
-	return fmt.Sprintf("Hello from basicSubController.\nRead-only visits count: %d", count)
+// 不遵循命名规范 BeforeActivation 中 b.Handle方法注册自定义路径
+// CustomHandlerWithoutFollowingTheNamingGuide 服务
+// 请求方法:   GET
+// 请求资源路径: http://localhost:8080/custom_path
+func (c *ExampleController) CustomHandlerWithoutFollowingTheNamingGuide() string {
+	return "hello from the custom handler without following the naming guide"
 }
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/kataras/iris/httptest"
+	"testing"
+)
+
+func TestMVCHelloWorld(t *testing.T) {
+	e := httptest.New(t, newApp())
+	e.GET("/").Expect().Status(httptest.StatusOK).ContentType("text/html", "utf-8").Body().Equal("<h1>Welcome</h1>")
+}
