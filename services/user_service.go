f3e2310230b338c5f62ada3adbb311c5a203f2c0
limq <limq@cgnfs.com.cn>
2019-08-20 17:32:40 +0800

    mvc login

--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"mvc/datamodels"
+	"mvc/repositories"
+)
+
+type UserService interface {
+	GetAll() []datamodels.User
+	GetByID(id int) (datamodels.User, bool)
+	GetByUsernameAndPassword(username, password string) (datamodels.User, bool)
+	DeleteByID(id int64) bool
+
+	Update(id int64, user datamodels.User) (datamodels.User, error)
+	UpdatePassword(id int64, newPassword string) (datamodels.User, error)
+	UpdateUsername(id int64, newUsername string) (datamodels.User, error)
+
+	Create(userPassword string, user datamodels.User) (datamodels.User, error)
+}
+
+type userService struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) UserService {
+	return &userService{repo: repo}
+}
